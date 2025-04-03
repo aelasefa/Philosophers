6b30797bbbfb899ec41e52cd4b1192506f6fce6c
aelasefa <ayman.zone.01@gmail.com>
2025-04-03 17:22:33 +0000

    add header phile

--- philo/philo.h
+++ philo/philo.h
@@ -1,7 +1,7 @@
 #ifndef PHILO_H
 #define PHLO_H
 
-
+#define MIN_PHILO 2
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -19,6 +19,15 @@ typedef struct s_input
 	int sleep_time;
 	int count_eat;
 }	t_input;
+
+typedef struct s_philosophers
+{
+	int id;
+	pthread_t thread;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+}	t_philosophers;
+
 int ft_atoi(char *str);
 
 #endif
