9d2c37c7963499f7b47a1f53b0c1b54ab3c3d22b
aelasefa <aymanelasefar@1337.com>
2025-04-09 12:03:57 +0100

    change ordre of init frunctions

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: ayelasef <ayelasef@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 16:22:12 by ayelasef          #+#    #+#             */
-/*   Updated: 2025/04/07 19:27:08 by ayelasef         ###   ########.fr       */
+/*   Updated: 2025/04/09 12:01:46 by ayelasef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,26 @@ int main(int ac, char **av)
 {
 	t_input	input;
 	int		i;
+	pthread_t	monitor;
 
 	if (!parse_input(ac, av, &input))
 		return (1);
-	if (init_philosophers(&input) || init_mutexes(&input))
+	if (init_mutexes(&input) || init_philosophers(&input))
 	{
-		free_input(&input);
+		printf("Exit\n");
 		return (1);
 	}
-		i = 0;
+	i = 0;
 	while (i < input.nbr_philo)
-		pthread_create(&input.philos[i].thread, NULL, &routine, &input.philos[i++]);
-
+	{
+		pthread_create(&input.philos[i].thread, NULL, &routine, &input.philos[i]);
+		i++;
+	}
 	pthread_create(&monitor, NULL, &death_monitor, &input);
 	pthread_join(monitor, NULL);
-
 	i = 0;
 	while (i < input.nbr_philo)
 		pthread_join(input.philos[i++].thread, NULL);
-
 	free_input(&input);
 	return (0);
-	if (start_simulation(&input))
-		return (1);
-	join_threads(&input);
 }
