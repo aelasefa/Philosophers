46a2e9fdca8736dfd18a686267b02d8da1f3b411
aelasefa <aymanelasefar1337@gmail.com>
2025-05-18 13:30:58 +0100

    fix odd number of philo

--- philo/actions.c
+++ philo/actions.c
@@ -63,28 +63,44 @@ int	philo_sleep(t_philo *philo)
 	ft_sleep(philo->input->sleep_time * 1000, philo);
 	return (1);
 }
-
-int	philo_think(t_philo *philo)
+int philo_think(t_philo *philo)
 {
-	t_input	*input;
-	long	time_to_die;
-	long	time_till_death;
-	long	time_to_eat;
-	long	time_to_sleep;
+    t_input *input = philo->input;
+    
+    // Check for simulation end or philosopher death
+    if (check_simulation_end(input) || philo_died(philo))
+        return (0);
 
-	input = philo->input;
-	if (check_simulation_end(input) || philo_died(philo))
-		return (0);
-	print_action(philo, "is thinking");
-	if (input->nbr_philo % 2 == 0)
-		return (1);
-	time_to_eat = input->eat_time;
-	time_to_sleep = input->sleep_time;
-	time_to_die = input->die_time;
-	if (time_to_sleep < time_to_eat)
-		ft_sleep((time_to_eat - time_to_sleep) * 1000, philo);
-	time_till_death = get_time() - philo->last_meal_time;
-	if (time_till_death < time_to_die * 0.7)
-		ft_sleep(1000, philo);
-	return (1);
+    print_action(philo, "is thinking");
+
+    if (input->nbr_philo % 2 == 1)
+    {
+        ft_sleep(input->eat_time / 2 * 1000, philo);
+    }
+
+    return (1);
 }
+/*int	philo_think(t_philo *philo)*/
+/*{*/
+/*	t_input	*input;*/
+/*	long	time_to_die;*/
+/*	long	time_till_death;*/
+/*	long	time_to_eat;*/
+/*	long	time_to_sleep;*/
+/**/
+/*	input = philo->input;*/
+/*	if (check_simulation_end(input) || philo_died(philo))*/
+/*		return (0);*/
+/*	print_action(philo, "is thinking");*/
+/*	if (input->nbr_philo % 2 == 0)*/
+/*		return (1);*/
+/*	time_to_eat = input->eat_time;*/
+/*	time_to_sleep = input->sleep_time;*/
+/*	time_to_die = input->die_time;*/
+/*	if (time_to_sleep < time_to_eat)*/
+/*		ft_sleep((time_to_eat - time_to_sleep) * 1000, philo);*/
+/*	time_till_death = get_time() - philo->last_meal_time;*/
+/*	if (time_till_death < time_to_die * 0.7)*/
+/*		ft_sleep(1000, philo);*/
+/*	return (1);*/
+/*}*/
